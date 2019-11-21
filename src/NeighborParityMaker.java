b9b116dd4a8d15e0217ae6bb1c1712513e2d1638
MrCatDog <failoff.catdog@yandex.ru>
2019-11-21 03:44:05 +0300

    Base structures changed. ArrayList's changed on simple array and HashSet's. Now all in one try-catch block. Works on easy test. Need to change switch block and exclude repetition code from it.

--- src/NeighborParityMaker.java
+++ src/NeighborParityMaker.java
@@ -5,115 +5,107 @@ import java.util.*;
 /**
  * Work in progress.
  */
-public class NeighborParityMaker {
+class NeighborParityMaker {
 
-    public static void makeParity(Path inputFile, Path outputFile) {
-        ArrayList<Integer> numbers = new ArrayList<>();
-        HashMap<Integer, Integer> evenElem = new HashMap<>();
-        HashMap<Integer,Integer> oddElem = new HashMap<>();
-        HashSet<Integer> used = new HashSet<>();
+    static void makeParity(Path inputFile, Path outputFile) {
+        Integer[] numbers;
+        HashSet<Integer> evenElem = new HashSet<>();
+        HashSet<Integer> oddElem = new HashSet<>();
 
-        boolean startSpace=false;
-        boolean endSpace=false;
+        boolean startSpace;
+        boolean endSpace;
 
-        try (Scanner in = new Scanner(inputFile)) {
-            int size = in.nextInt();
+        try (Scanner in = new Scanner(inputFile);
+             Writer out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputFile.toFile()), java.nio.charset.StandardCharsets.UTF_8)))
+        {
+            numbers = new Integer[in.nextInt()];
             int number = in.nextInt();
-
             boolean parity = number % 2 == 0;
-
             startSpace=!parity;
+            numbers[0]=number;
 
-            numbers.add(number);
-
-            for (int i = 1; i < size; i++) {
+            for (int i = 1; i < numbers.length; i++) {
                 number = in.nextInt();
 
                 if ((number % 2 == 0) == parity) {
                     if (parity)
-                        evenElem.put(i,number);
+                        evenElem.add(i);
                     else
-                        oddElem.put(i,number);
+                        oddElem.add(i);
                 } else
                     parity=!parity;
 
-                numbers.add(number);
+                numbers[i] = number;
             }
-
             endSpace=!parity;
-        } catch (IOException ex) {
-            System.out.println(ex.toString());
-            System.exit(-1);
-        }
 
-        try (Writer out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputFile.toFile()), java.nio.charset.StandardCharsets.UTF_8))) {
-
+            Iterator<Integer> iterator;
             int sub=evenElem.size() - oddElem.size();
-            Integer num;
             switch (sub) {
                 case 0:
-                    //write
+                    iterator=evenElem.iterator();
+                    write(out,numbers,evenElem,oddElem,iterator);
                     break;
 
                 case 1:
-                    if(startSpace) {
-                        num=evenElem.remove(0);
-                        out.write(num);
-                        used.add(num);
-                        write(out,numbers,oddElem,evenElem,used);
+                    if(evenElem.size()>oddElem.size()) {
+                        iterator = evenElem.iterator();
+                    } else {
+                        iterator = oddElem.iterator();
+                        startSpace=!startSpace;
+                        endSpace=!endSpace;
+                    }
+                    if(startSpace || endSpace) {
+                        boolean insertFlag=false;
+                        if(startSpace) {
+                            out.write(numbers[iterator.next()] + " ");
+                            insertFlag=true;
+                        }
+                        write(out,numbers,evenElem,oddElem,iterator);
+                        if(!insertFlag)
+                            out.write(numbers[iterator.next()] + " ");
                         break;
                     }
-                    if(endSpace) {
-                        num=evenElem.remove(0);
-                        used.add(num);
-                        write(out,numbers,oddElem,evenElem,used);
-                        out.write(num);
+                    else {
+                        out.write(-1);//no space for one odd element
                         break;
                     }
-                    out.write(-1);//no space for one even element
-                    break;
 
                 case 2:
                     if(startSpace && endSpace) {
-                        num=evenElem.remove(0);
-                        out.write(numbers.get(num));
-                        used.add(num);
-                        num=evenElem.remove(0);
-                        used.add(num);
-                        write(out,numbers,oddElem,evenElem,used);
-                        out.write(numbers.get(num));
+                        iterator=evenElem.iterator();
+                        out.write(numbers[iterator.next()]);
+                        write(out,numbers,oddElem,evenElem,iterator);
+                        out.write(numbers[iterator.next()]);
                         break;
                     }
                     else
                         out.write(-1);//No space for two even elements
 
                 case -1:
-                    if(!startSpace) {
-                        num=oddElem.remove(0);
-                        out.write(numbers.get(num)+" ");
-                        used.add(num);
-                        write(out,numbers,evenElem,oddElem,used);
+                    if(!startSpace || !endSpace) {
+                        iterator=oddElem.iterator();
+                        boolean insertFlag=false;
+                        if(!startSpace) {
+                            out.write(numbers[iterator.next()] + " ");
+                            insertFlag=true;
+                        }
+                        write(out,numbers,evenElem,oddElem,iterator);
+                        if(!insertFlag)
+                            out.write(numbers[iterator.next()] + " ");
                         break;
                     }
-                    if(!endSpace) {
-                        num=oddElem.remove(0);
-                        used.add(num);
-                        write(out,numbers,evenElem,oddElem,used);
-                        out.write(numbers.get(num));
+                    else {
+                        out.write(-1);//no space for one odd element
                         break;
                     }
-                    out.write(-1);//no space for one odd element
-                    break;
 
                 case -2:
                     if(!startSpace && !endSpace) {
-                        num=oddElem.remove(0);
-                        out.write(numbers.get(num));
-                        used.add(num);
-                        num=oddElem.remove(0);
-                        used.add(num);
-                        write(out,numbers,evenElem,oddElem,used);
-                        out.write(numbers.get(num));
+                        iterator=oddElem.iterator();
+                        out.write(numbers[iterator.next()]);
+                        write(out,numbers,evenElem,oddElem,iterator);
+                        out.write(numbers[iterator.next()]);
                         break;
                     }
                     else
@@ -130,23 +122,18 @@ public class NeighborParityMaker {
 
     }
 
-    private static void write(Writer out, ArrayList<Integer> numbers, HashMap<Integer, Integer> smallest, HashMap<Integer, Integer> biggest, HashSet<Integer> used) throws IOException
+    private static void write(Writer out, Integer[] numbers, HashSet<Integer> smallest, HashSet<Integer> biggest, Iterator<Integer> iterator) throws IOException
     {
-        ListIterator<Integer> iterator = numbers.listIterator();
-        Integer numberBuff;
-        while (iterator.hasNext()) {
-            int nextIndex = iterator.nextIndex();
-            if (smallest.containsKey(nextIndex)) {
-                numberBuff=iteratorBig.next();
-                iteratorBig.remove();
-                out.write(numbers.get(numberBuff)+" ");//WARNING if you delete space you will use write(int), and that will write symbols, NaN. Try append.
-                used.add(numberBuff);
-                out.write(iterator.next()+" ");
+        out.write(numbers[0]+" ");
+        int nextIndex;
+        for (int i=0;i<numbers.length-1;i++) {
+            nextIndex=i+1;
+            if (smallest.contains(nextIndex)) {
+                out.write(numbers[iterator.next()]+" ");//WARNING if you delete space you will use write(int), and that will write symbols, NaN. Try append.
+                out.write(numbers[nextIndex]+" ");
             } else {
-                if(biggest.containsKey(nextIndex) || used.contains(nextIndex))
-                    iterator.next();
-                else
-                    out.write(iterator.next()+" ");
+                if(!biggest.contains(nextIndex))
+                    out.write(numbers[i+1]+" ");
             }
         }
     }
