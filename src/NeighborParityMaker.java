a507c225821df972cb04e4be6a79cd66ef55549c
MrCatDog <failoff.catdog@yandex.ru>
2019-12-01 23:54:09 +0300

    Added JavaDoc. Switch optimized. No more duplicated code. Method "write" now uses append when it needed. Comments added.

--- src/NeighborParityMaker.java
+++ src/NeighborParityMaker.java
@@ -3,10 +3,20 @@ import java.nio.file.Path;
 import java.util.*;
 
 /**
- * Work in progress.
+ * Read integer sequence from file and, if it possible, write a number of transpositions and a new sequence into new file,
+ * where all neighbors for all numbers have opposite parity.
+ * Algorithm made to make minimum transpositions in input sequence.
+ * If changes are possible, writes a count of transpositions and, in a next string, new built sequence.
+ * If changes are not possible, writes "-1".
+ * @author Konstantin
+ * @version 1.0
  */
 class NeighborParityMaker {
-
+    /**
+     * Main method. Call it for make your sequence "parity-changed".
+     * @param inputFile input file path. First line should be a number of integers in sequence. Second should be an integer sequence.
+     * @param outputFile output file path. It will be created if not exists.
+     */
     static void makeParity(Path inputFile, Path outputFile) {
         Integer[] numbers;
         HashSet<Integer> evenElem = new HashSet<>();
@@ -39,101 +49,87 @@ class NeighborParityMaker {
             }
             endSpace=!parity;
 
+            //this block define biggest Set and change settings for it
             Iterator<Integer> iterator;
-            int sub=evenElem.size() - oddElem.size();
-            switch (sub) {
+            HashSet<Integer> big;
+            HashSet<Integer> little;
+            if(oddElem.size()>evenElem.size()) {
+                startSpace=!startSpace;
+                endSpace=!endSpace;
+                iterator=oddElem.iterator();
+                big=oddElem;
+                little=evenElem;
+            } else {
+                iterator=evenElem.iterator();
+                big=evenElem;
+                little=oddElem;
+            }
+
+            //this block define difference between sets, check free space for unpaired elements and starts a write operation
+            switch (big.size() - little.size()) {
                 case 0:
-                    iterator=evenElem.iterator();
-                    write(out,numbers,evenElem,oddElem,iterator);
+                    out.write(big.size()+"\n");//write count of transpositions. Only elements from bigger Set will change their places, so size is a count.
+                    write(out,numbers,little,big,iterator);
                     break;
 
                 case 1:
-                    if(evenElem.size()>oddElem.size()) {
-                        iterator = evenElem.iterator();
-                    } else {
-                        iterator = oddElem.iterator();
-                        startSpace=!startSpace;
-                        endSpace=!endSpace;
-                    }
                     if(startSpace || endSpace) {
-                        boolean insertFlag=false;
-                        if(startSpace) {
-                            out.write(numbers[iterator.next()] + " ");
-                            insertFlag=true;
+                        out.write(big.size()+"\n");//write count of transpositions. Only elements from bigger Set will change their places, so size is a count.
+                        if(startSpace) {//if start pos assume required parity
+                            out.write(numbers[iterator.next()] + " ");//write it on start pos
+                            write(out,numbers,little,big,iterator);//write new sequence.
                         }
-                        write(out,numbers,evenElem,oddElem,iterator);
-                        if(!insertFlag)
-                            out.write(numbers[iterator.next()] + " ");
-                        break;
+                        else {
+                            write(out,numbers,evenElem,oddElem,iterator);//write new sequence.
+                            out.write(numbers[iterator.next()] + " ");//write extra elem to end pos
+                        }
                     }
-                    else {
-                        out.write(-1);//no space for one odd element
-                        break;
-                    }
+                    else
+                        out.write("-1");//no space for one element
+                    break;
 
                 case 2:
-                    if(startSpace && endSpace) {
-                        iterator=evenElem.iterator();
-                        out.write(numbers[iterator.next()]);
-                        write(out,numbers,oddElem,evenElem,iterator);
-                        out.write(numbers[iterator.next()]);
-                        break;
+                    if(startSpace && endSpace) {//if we have enough places for two elements
+                        out.write(big.size()+"\n");//write count of transpositions. Only elements from bigger Set will change their places, so size is a count.
+                        out.write(numbers[iterator.next()]+" ");//write first elem on start pos.
+                        write(out,numbers,little,big,iterator);//write new sequence.
+                        out.write(numbers[iterator.next()]+"");//write last elem on end pos.
                     }
                     else
-                        out.write(-1);//No space for two even elements
-
-                case -1:
-                    if(!startSpace || !endSpace) {
-                        iterator=oddElem.iterator();
-                        boolean insertFlag=false;
-                        if(!startSpace) {
-                            out.write(numbers[iterator.next()] + " ");
-                            insertFlag=true;
-                        }
-                        write(out,numbers,evenElem,oddElem,iterator);
-                        if(!insertFlag)
-                            out.write(numbers[iterator.next()] + " ");
-                        break;
-                    }
-                    else {
-                        out.write(-1);//no space for one odd element
-                        break;
-                    }
-
-                case -2:
-                    if(!startSpace && !endSpace) {
-                        iterator=oddElem.iterator();
-                        out.write(numbers[iterator.next()]);
-                        write(out,numbers,evenElem,oddElem,iterator);
-                        out.write(numbers[iterator.next()]);
-                        break;
-                    }
-                    else
-                        out.write(-1);//No space for two odd elements
+                        out.write("-1");//No space for two elements
+                    break;
 
                 default:
-                    out.write(-1);//Too many elements with same parity.
+                    out.write("-1");//Too many elements with same parity.
                     break;
                 }
         } catch (IOException ex) {
             System.out.println(ex.toString());
             System.exit(-1);
         }
-
     }
 
-    private static void write(Writer out, Integer[] numbers, HashSet<Integer> smallest, HashSet<Integer> biggest, Iterator<Integer> iterator) throws IOException
+    /**
+     * Write input sequence into out file in right parity order.
+     * @param out output file.
+     * @param input input sequence.
+     * @param wrongIndexes set of wrong indexes, which need to be replaced.
+     * @param iteratedWrongIndexes set of wrong indexes, but bigger or equal wrongIndexes and iterated by iterator.
+     * @param iterator iterator of iteratedWrongIndexes which show the next element to insert into sequence.
+     * @throws IOException if an output error occurs while file writing.
+     */
+    private static void write(Writer out, Integer[] input, HashSet<Integer> wrongIndexes, HashSet<Integer> iteratedWrongIndexes, Iterator<Integer> iterator) throws IOException
     {
-        out.write(numbers[0]+" ");
+        out.write(input[0]+" ");//Writes bearing element, which cannot be in sets
         int nextIndex;
-        for (int i=0;i<numbers.length-1;i++) {
+        for (int i = 0; i< input.length-1; i++) {
             nextIndex=i+1;
-            if (smallest.contains(nextIndex)) {
-                out.write(numbers[iterator.next()]+" ");//WARNING if you delete space you will use write(int), and that will write symbols, NaN. Try append.
-                out.write(numbers[nextIndex]+" ");
+            if (wrongIndexes.contains(nextIndex)) {//if wrongIndexes contains this element,
+                out.append(input[iterator.next()].toString()).append(' ');//write into a new sequence an element from iteratedWrongIndexes
+                out.append(input[nextIndex].toString()).append(' ');//and this elem after what.
             } else {
-                if(!biggest.contains(nextIndex))
-                    out.write(numbers[i+1]+" ");
+                if(!iteratedWrongIndexes.contains(nextIndex))//all elements from iteratedWrongIndexes must change their position, so we skip them in input sequence.
+                    out.append(input[i+1].toString()).append(' ');//all this elements had a rightful position at start. Just rewrite them into a new sequence.
             }
         }
     }
